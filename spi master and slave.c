98ceb20d5f4c8b5b3f0b32ad1ce02f2aabfe37c0
santhitanneedi <50623059+santhitanneedi@users.noreply.github.com>
2019-05-17 17:45:44 +0530

    Add files via upload

---
+++ spi master and slave.c
@@ -0,0 +1,18 @@
+/*
+ * SPI master and slave.c
+ *
+ * Created: 17-May-19 11:53:24 AM
+ * Author : ALEKHYA
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
